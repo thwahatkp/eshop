01638c4a7a3f080e4f0cf2de78d42e4d6e9bfb8f
thwahatkp <thwahatkp@gmail.com>
2023-04-16 22:55:35 +0530

    added product section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
+import ProductSection from "./components/ProductSection";
 
 function App() {
   return (
     <div className="container h-8 mx-auto p-5">
       <Header />
       <HeroSection/>
-
+      <ProductSection/>
     </div>
   );
 }
