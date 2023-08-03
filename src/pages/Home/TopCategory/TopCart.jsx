895344b5e3eb8402b2d50b7918ffc271150704af
thwahatkp <thwahatkp@gmail.com>
2023-08-03 20:41:39 +0530

    fixed cart

--- src/pages/Home/TopCategory/TopCart.jsx
+++ src/pages/Home/TopCategory/TopCart.jsx
@@ -78,38 +78,20 @@ const TopCart = () => {
   return (
     <div className="overflow-hidden">
       <Slider {...settings}>
-      <div
-      className="box product bg-white p-5 relative rounded-md shadow m-5 flex flex-col items-center justify-center transition ease-in-out duration-500 hover:scale-105 cursor-pointer"
-    >
-            <div className="nametop d_flex relative w-full flex justify-between">
-        <Skeleton
-          variant="text"
-          width="40%"
-          height={20}
-          style={{ marginBottom: "5px" }}
-        />
-        <Skeleton
-          variant="text"
-          width="40%"
-          height={20}
-          style={{ marginBottom: "5px" }}
-        />
-      </div>
-      <div className="img grow relative rounded-lg">
-        <Skeleton
-          variant="rectangular"
-          width="100%"
-          height={81}
-          style={{ marginBottom: "10px", borderRadius: "8px" }}
-        />
-      </div>
-    </div>
+        <div className="box product bg-white p-5 relative rounded-md shadow m-5 flex flex-col items-center justify-center transition ease-in-out duration-500 hover:scale-105 cursor-pointer">
+          <div className="nametop d_flex relative w-full flex justify-between">
+            <Skeleton variant="text" width="40%" height={20} style={{ marginBottom: "5px" }} />
+            <Skeleton variant="text" width="40%" height={20} style={{ marginBottom: "5px" }} />
+          </div>
+          <div className="img grow relative rounded-lg">
+            <Skeleton variant="rectangular" width="100%" height={81} style={{ marginBottom: "10px", borderRadius: "8px" }} />
+          </div>
+        </div>
         {Tdata.map((value, index) => {
           return (
             <div
               key={index}
-              className="box product bg-white p-5 relative rounded-md shadow m-5 flex flex-col items-center justify-center transition ease-in-out duration-500 hover:scale-105 cursor-pointer"
-            >
+              className="box product bg-white p-5 relative rounded-md shadow m-5 flex flex-col items-center justify-center transition ease-in-out duration-500 hover:scale-105 cursor-pointer">
               {/* <div className="nametop d_flex relative w-full flex justify-between"></div> */}
               <div className="img grow relative rounded-lg">
                 <span className="tleft bg-[#0f3460] text-white text-[12px] py-1.5 px-2.5 rounded-[50px] m-1 absolute cursor-pointer top-2 ">
@@ -118,7 +100,7 @@ const TopCart = () => {
                 <span className="tright bg-[aliceblue] text-black text-[12px] py-1.5 px-2.5 rounded-[50px] m-1 absolute cursor-pointer right-0 top-2">
                   {value.desc}
                 </span>
-                <img className="rounded-md" src={value.cover} alt="" />
+                <img loading="lazy" className="rounded-md" src={value.cover} alt="" />
               </div>
             </div>
           );
